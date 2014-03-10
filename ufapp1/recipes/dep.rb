cf7ac58972279a02bb9009dcb5c8098a1d5602a2
ntaguemount <ntaguemount@universalflower.com>
2014-03-10 17:49:57 +0100

    testdep3

--- ufapp1/recipes/dep.rb
+++ ufapp1/recipes/dep.rb
@@ -1,3 +1,9 @@
-deploy "/WEBS/universalflower/public/app" do
-  repo "git://github.com/ntaguemount/nt-cookbooks.git"
+#deploy "/WEBS/universalflower/public/app" do
+#  repo "git://github.com/ntaguemount/nt-cookbooks.git"
+#end
+
+git "/WEBS/universalflower/public/app" do
+  repository "git://github.com/ntaguemount/nt-cookbooks.git"
+  action :sync
+
 end
