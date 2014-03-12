813238c2b8b619a277c14585671a13fa3859d49f
ntaguemount <ntaguemount@universalflower.com>
2014-03-12 10:29:14 +0100

    testservice

--- ufapp1/recipes/ufservicetest.rb
+++ ufapp1/recipes/ufservicetest.rb
@@ -1,7 +1,11 @@
-service "apache2" do
-	action :stop
-end
+#service "apache2" do
+#	action :stop
+#end
+
+#service "apache2" do
+#	action :start
+#end
 
 service "apache2" do
-	action :start
+	action :restart
 end
