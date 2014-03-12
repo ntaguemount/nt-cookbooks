41f6271279fca7d5433118df3716ff1102b92a31
ntaguemount <ntaguemount@universalflower.com>
2014-03-12 12:19:38 +0100

    testservice2

--- ufapp1/recipes/ufservicetest.rb
+++ ufapp1/recipes/ufservicetest.rb
@@ -9,3 +9,15 @@
 service "apache2" do
 	action :restart
 end
+
+
+my_bag = data_bag_item("apps", "my_app")
+
+
+bash "install_gag_test" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  echo my_bag["repository"] > /tmp/aaa.txt
+  EOH
+end
